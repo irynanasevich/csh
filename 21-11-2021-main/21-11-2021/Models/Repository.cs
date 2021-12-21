8ad23ea3d281fc1033c79dca261cade788379e37
irynanasevich <59256323+irynanasevich@users.noreply.github.com>
2021-12-21 14:38:18 +0300

    Add files via upload

---
+++ 21-11-2021-main/21-11-2021/Models/Repository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace _21_11_2021.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
